22d073e96ba2b2f4ff9b313b2d2e69db190279af
Jorge Juan Rodríguez Manzano <jromanz@gmail.com>
2021-11-19 13:46:18 +0100

    Initial commit

---
+++ src/main/java/org/acme/springnativewillycoyote/SpringNativeWillyCoyoteApplication.java
@@ -0,0 +1,13 @@
+package org.acme.springnativewillycoyote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringNativeWillyCoyoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringNativeWillyCoyoteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/acme/springnativewillycoyote/SpringNativeWillyCoyoteApplicationTests.java
@@ -0,0 +1,13 @@
+package org.acme.springnativewillycoyote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringNativeWillyCoyoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
